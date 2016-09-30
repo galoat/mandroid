8cbf34c65e3fa332e961b0a62d20b3cfe82cc851
lo52tp <lo52tp@lo52tp-VirtualBox.(none)>
2016-09-30 14:34:20 +0200

    Creation des 5 premiers fichiers

---
+++ mandroid.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/linaro/pandaboard/pandaboard.mk)
+
+PRODUCT_NAME:= mandroid
+PRODUCT_DEVICE:= mandroid
+PRODUCT_BRAND:= Android
+PRODUCT_MODEL:= Android
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo mandroid-eng
+add_lunch_combo mandroid-user
+add_lunch_combo mandroid-userdebug
